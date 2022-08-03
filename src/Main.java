0d872f6bc6624c21b2682c3c15651dcea3774bf8
Ed <edwardbromilow@live.co.uk>
2022-08-03 16:22:56 +0100

    not sure that worked

--- src/Main.java
+++ src/Main.java
@@ -63,7 +63,7 @@ public class Main
         calcPrice(litresNeeded(wallSize(length, width, height, coats))); // call the calculate price procedure
 
         displayPrice(calcPrice(litresNeeded(wallSize(length, width, height, coats)))); // display the final price based on the above results
-        //so if I change this now? is it on github?
+        //so if I change this now? it works?
 
     }
 }
