46d2d17c1652a0feb54d156e8d3aafdbb7a6a2bc
Ed <edwardbromilow@live.co.uk>
2022-08-03 16:28:28 +0100

    attempt via cmd

--- src/Main.java
+++ src/Main.java
@@ -63,7 +63,7 @@ public class Main
         calcPrice(litresNeeded(wallSize(length, width, height, coats))); // call the calculate price procedure
 
         displayPrice(calcPrice(litresNeeded(wallSize(length, width, height, coats)))); // display the final price based on the above results
-        //I fixed the GitHub REPO
+        //I fixed the GitHub attempting now via cmd line
 
     }
 }
