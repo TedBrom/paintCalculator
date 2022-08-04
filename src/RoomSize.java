28f786c40c09e5f09901fd3eea36deaecb343b8e
Ed <edwardbromilow@live.co.uk>
2022-08-04 12:59:43 +0100

    begun creating object classes

--- src/RoomSize.java
+++ src/RoomSize.java
@@ -1,4 +1,19 @@
+
+import java.util.Scanner;
 public class RoomSize
 {
+        double length;
+        double height;
+        double walls;
 
+        public RoomSize(double givenLength, double givenHeight, double numWalls)
+        {
+            length = givenLength;
+            height = givenHeight;
+            walls = numWalls;
+        }
+        public static void main (String[] args)
+        {
+
+        }
 }
